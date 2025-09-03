4319c28c6e8edb01c33cc465a12657c7d10f93c3
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-03 20:56:03 +0300

    auth updated when user signup token will be signed

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,7 +23,13 @@ exports.register = async(req, res)=>{
         const newUser = new User({username, email, password : passwordHash})
         await newUser.save()
 
-        res.status(201).json({message: 'User registered successfully'})
+        const payload = {
+           id: newUser._id
+        }
+
+        const token = jwt.sign(payload, SECRET, {expiresIn: '1d'})
+        
+        res.status(201).json({message: 'User registered successfully', token})
     } catch (err) {
         res.status(500).json({message: err.message})
     }
