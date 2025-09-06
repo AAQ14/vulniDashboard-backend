0ffb6585b45d2795559d014ff8edddcf1e0170f1
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-06 20:10:21 +0300

    increase apps or infrastructure added

--- controllers/system.js
+++ controllers/system.js
@@ -2,57 +2,82 @@ const System = require("../models/System")
 const Asset = require("../models/Asset")
 const Vuln = require("../models/Vulnerability")
 
-const systemIndex = async(req,res) =>{
+const systemIndex = async (req, res) => {
     try {
         const allsystems = await System.find()
-        if(allsystems.length)
+        if (allsystems.length)
             res.status(200).json(allsystems)
         else
             res.sendStatus(404)
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 }
 
-const createSystem = async(req,res) =>{
+const createSystem = async (req, res) => {
     try {
         const created = await System.create(req.body)
         res.status(201).json(created)
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 }
 
-async function test(){
+async function test() {
     const assets = await Asset.find()
-    console.log(assets)
+    // console.log(assets)
     let low = 0
     assets.forEach(asset => {
-       low += asset.vulnerabilities.low
+        low += asset.vulnerabilities.low
     })
-    console.log(low)
-    // const vulns = await Vuln.find()
-    // console.log(vulns.length)
+    // console.log(low)
+    const vulns = await Vuln.find()
+    // console.log(vulns)
+    let openVuln = 0
+    console.log(openVuln)
+    vulns.forEach(vul => {
+        if (vul.status === "Open") {
+            openVuln += 1
+        }
+    })
+    console.log(openVuln)
+
+
 }
 
 test()
 
-const updateSystem = async (req,res) =>{
+const updateSystem = async (req, res) => {
     try {
         const system = await System.findById(req.params.id)
         const vulns = await Vuln.find()
         const assets = await Asset.find()
         system.vulnerabilities = vulns.length
 
-        assets.forEach(asset =>{
+        assets.forEach(asset => {
             system.lowSeverityVulns += asset.vulnerabilities.low
             system.lowSeverityVulns += asset.vulnerabilities.medium
             system.lowSeverityVulns += asset.vulnerabilities.high
             system.lowSeverityVulns += asset.vulnerabilities.critical
+            if(asset.type === 'Web App' ||asset.type === 'Mobile App' ||asset.type === 'Desktop Software') 
+                system.applications += 1
+            else
+                system.infrastructures += 1
         })
-        
+
+        vulns.forEach(vul => {
+            if (vul.status === "Open")
+                system.openVulns += 1
+            else if (vul.status === "Fixed")
+                system.fixedVulns += 1
+            else if (vul.status === "In progress")
+                system.inProgressVulns += 1
+        })
+
+        system.save()
+
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 }
 
